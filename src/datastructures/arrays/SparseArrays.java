30b976c086b6ac2c0ffe031bee2fbefbaa2ecc25
Avelpa <game.player212@gmail.com>
2016-02-21 22:03:50 -0500

    arrays started harder ones

---
+++ src/datastructures/arrays/SparseArrays.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package datastructures.arrays;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Dmitry
+ */
+public class SparseArrays {
+
+    static Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        int numTests = in.nextInt();
+        while(numTests-- > 0){
+            solve();
+        }
+    }
+    
+    private static void solve()
+    {
+        
+    }
+    
+}
