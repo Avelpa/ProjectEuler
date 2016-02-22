d260861683cddc0fd0bd1e75a0974337ff37beff
kobed6328 <kobed6328@HRH-IC0027289.wrdsb.ca>
2016-02-22 14:24:44 -0500

    arrays hard not really hard, but getting wrong answer.......

---
+++ src/datastructures/arrays/DynamicArray.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datastructures.arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author kobed6328
+ */
+public class DynamicArray {
+    static Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        solve();
+    }
+    
+    private static void solve()
+    {
+        final int N = in.nextInt();
+        int q = in.nextInt();
+        
+        ArrayList<Integer>[] arrs = new ArrayList[N];
+        Arrays.fill(arrs, new ArrayList<Integer>());
+        
+        int lastans = 0;
+        
+        while (q-- > 0)
+        {
+            int type = in.nextInt();
+            int x = in.nextInt(2);
+            int y = in.nextInt();
+            
+            if (type == 1)
+                arrs[(x^lastans)%N].add(y);
+            else if (type == 2)
+            {
+                int answer = arrs[(x^lastans)%N].get(y%arrs[(x^lastans)%N].size());
+                System.out.println(answer);
+                lastans = answer;
+            }
+        }
+    }
+}
--- src/datastructures/arrays/SparseArrays.java
+++ src/datastructures/arrays/SparseArrays.java
@@ -6,26 +6,44 @@
 
 package datastructures.arrays;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
  *
- * @author Dmitry
+ * @author Dmitry   
  */
 public class SparseArrays {
 
     static Scanner in = new Scanner(System.in);
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        int numTests = in.nextInt();
-        while(numTests-- > 0){
-            solve();
-        }
+        solve();
     }
     
     private static void solve()
     {
+        int n = in.nextInt();
         
+        HashMap<String, Integer> strs = new HashMap();
+        
+        while (n-- > 0)
+        {
+            String word = in.next();
+            if (strs.containsKey(word))
+                strs.put(word, strs.get(word)+1);
+            else
+                strs.put(word, 1);
+        }
+        
+        int q = in.nextInt();
+        while (q-- > 0)
+        {
+            String search = in.next();
+            if (strs.containsKey(search))
+                System.out.println(strs.get(search));
+            else
+                System.out.println(0);
+        }
     }
-    
 }
